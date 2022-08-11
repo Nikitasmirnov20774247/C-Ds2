6f0ef2b59d82321689dcbd2c0982066c3f6e515b
Nikita <Nikitasmirnov2000@yandex.ru>
2022-08-11 10:53:49 +0300

    ds1

--- Ds1/Program.cs
+++ Ds1/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введи трёхзначное число: ");
+int Number = Convert.ToInt32(Console.ReadLine());
+string Num2 = Convert.ToString(Number);
+
+if (Num2.Length <= 2 || Num2.Length > 3)
+{
+    Console.WriteLine("!! Введённое число не является трёхзначным !!");
+}
+
+else if (Num2.Length > 2)
+{
+    Console.WriteLine("Вторая цифра введённого трёхзначного числа: " + Num2[1]);
+}
