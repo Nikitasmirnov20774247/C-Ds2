edf9240de75ea276063ccbc4ce99783f84bd0e4c
Nikita <Nikitasmirnov2000@yandex.ru>
2022-08-11 11:00:49 +0300

    ds3

---
+++ Ds3/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Write("Введи цифру, обозначающую день недели: ");
+int dayNumber = Convert.ToInt32(Console.ReadLine());
+
+void День_недели (int dayNumber) 
+{
+  if (dayNumber == 6 || dayNumber == 7) 
+  {
+  Console.WriteLine("Этот день выходной? -- да");
+  }
+  else if (dayNumber < 1 || dayNumber > 7) 
+  {
+    Console.WriteLine("!! Такого дня нет !!");
+  }
+  else Console.WriteLine("Этот день выходной? -- нет");
+}
+
+День_недели (dayNumber);
