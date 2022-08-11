e51b768964ebff282ece5956b54e67daec0ba4e0
Nikita <Nikitasmirnov2000@yandex.ru>
2022-08-11 11:00:18 +0300

    ds2

---
+++ Ds2/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введи число: ");
+int Number = Convert.ToInt32(Console.ReadLine());
+string FinelNumber = Convert.ToString(Number);
+
+if (FinelNumber.Length <= 2)
+{
+  Console.WriteLine("!! третьей цифры нет !!");
+}
+
+else if (FinelNumber.Length > 2)
+{
+  Console.WriteLine("третья цифра -> " + FinelNumber[2]);
+}
